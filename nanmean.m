3ef6b0e7f586c4e52b67d446e23ed1734027b152
gewimmer-neuro <35133192+gewimmer-neuro@users.noreply.github.com>
2020-03-07 13:30:08 +0000

    Add files via upload

---
+++ nanmean.m
@@ -0,0 +1,17 @@
+function y = nanmean(varargin)
+%NANMEAN Mean value, ignoring NaNs.
+%   M = NANMEAN(X) returns the sample mean of X, treating NaNs as missing
+%   values.  For vector input, M is the mean value of the non-NaN elements
+%   in X.  For matrix input, M is a row vector containing the mean value of
+%   non-NaN elements in each column.  For N-D arrays, NANMEAN operates
+%   along the first non-singleton dimension.
+%
+%   NANMEAN(X,DIM) takes the mean along dimension DIM of X.
+%
+%   See also MEAN, NANMEDIAN, NANSTD, NANVAR, NANMIN, NANMAX, NANSUM.
+
+%   Copyright 1993-2016 The MathWorks, Inc.
+
+
+narginchk(1,2);
+y = mean(varargin{:},'omitnan');
